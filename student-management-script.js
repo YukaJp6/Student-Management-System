1220b8547834c194da3e37a99dd64b5c32873738
Greg <projectyuka86@gmail.com>
2025-04-29 13:17:00 -0400

    2nd commit

--- student-management-script.js
+++ student-management-script.js
@@ -1,1 +1,33 @@
-const gg = 2
+// students array
+
+const students = [];
+
+
+//  function for adding students
+function addStudent(name, grade) {
+    students.push ({
+        name,
+        grade,
+    });
+}
+
+// function to remove students
+function removeStudent(name) {
+    const index = students.findIndex((student) => student.name === name);
+    if (index !== -1) {
+        students.splice(index, 1);
+        console.log(name, `has been removed`);
+    } else {
+        console.log(name, `was not found`);
+    }
+}
+
+// function to filter through top students
+function filterTopStudents(minGrade) {
+    return students.filter(student => student.grade >= minGrade);
+}
+
+// function to map students into formatted list
+function formatStudents() {
+    return students.map(student => `${student.name} - Grade: ${student.grade}`);
+}
